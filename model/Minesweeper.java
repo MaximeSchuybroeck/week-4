a9b8029835a68884605742243b3419bb3a7414fc
WoutC <wout.courtin@outlook.com>
2022-03-07 16:22:35 +0100

    version3-Wout

--- model/Minesweeper.java
+++ model/Minesweeper.java
@@ -1,9 +1,74 @@
 package model;
 
-public class Minesweeper extends AbstractMinesweeper{
+public class Minesweeper extends AbstractMineSweeper{
 
 
     public Minesweeper(){
 
+    }
+
+    @java.lang.Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @java.lang.Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @java.lang.Override
+    public void startNewGame(Difficulty level) {
+
+    }
+
+    @java.lang.Override
+    public void startNewGame(int row, int col, int explosionCount) {
+
+    }
+
+    @java.lang.Override
+    public void toggleFlag(int x, int y) {
+
+    }
+
+    @java.lang.Override
+    public AbstractTile getTile(int x, int y) {
+        return null;
+    }
+
+    @java.lang.Override
+    public void setWorld(AbstractTile[][] world) {
+
+    }
+
+    @java.lang.Override
+    public void open(int x, int y) {
+
+    }
+
+    @java.lang.Override
+    public void flag(int x, int y) {
+
+    }
+
+    @java.lang.Override
+    public void unflag(int x, int y) {
+
+    }
+
+    @java.lang.Override
+    public void deactivateFirstTileRule() {
+
+    }
+
+    @java.lang.Override
+    public AbstractTile generateEmptyTile() {
+        return null;
+    }
+
+    @java.lang.Override
+    public AbstractTile generateExplosiveTile() {
+        return null;
     }
 }
